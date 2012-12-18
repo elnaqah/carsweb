cd5f6953f8b9e44f92bd4b92a89749067583a1c6
elnaqah <eng.ahmed.medhat@gmail.com>
2012-12-18 23:19:36 +0200

    initializing online repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carsweb::Application.config.secret_token = '4aa5afa1d25144ab06b9116290a7f4b66471435386c37bc9d7a1d4ad49a31396b136cd02159ace65f04828dfb23d916c5d86daf6d86e97ee1587bd6aed0cb5b0'
