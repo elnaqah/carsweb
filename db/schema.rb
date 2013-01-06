47166a1d6ec07bc565ca612e14275ead7f0be062
Ahmedelhady <ah.melscor@gmail.com>
2013-01-06 19:45:54 +0200

    Facebook authentication and showing user image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130106150128) do
+ActiveRecord::Schema.define(:version => 20130106172027) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "owner_id"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20130106150128) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "image"
   end
 
 end
